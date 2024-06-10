256a548cad276f45288f89a72d3449a98b1b8888
Benjamin Pmt <benjamin.pimont@laposte.net>
2024-06-10 19:31:53 +0200

    add gitignore

--- script.js
+++ script.js
@@ -5,20 +5,15 @@ const btnReset = document.querySelector('.img_btn')
 // Variable pour stocker la note sélectionnée
 let selectedRating = 0;
 
-// Ajouter des écouteurs d'événements pour chaque étoile
-stars.forEach((star, index) => {
-  star.addEventListener('mouseover', () => handleMouseOver(index));
-  star.addEventListener('mouseout', handleMouseOut);
-  star.addEventListener('click', () => handleClick(index));
-});
-
 // Fonction pour gérer le survol de la souris
 function handleMouseOver(index) {
   // Change les étoiles jusqu'à celle survolée
-  for (let i = 0; i <= index; i++) {
-    stars[i].src = './img/star_full.png';
-    if(index === i){
-        stars[i].src= './img/star_hover.png'
+  for (let i = 0; i < stars.length; i++) {
+    if (i <= index) {
+      stars[i].src = './img/star_full.png';
+    }
+    if (i === index) {
+      stars[i].src = './img/star_hover.png';
     }
   }
 }
@@ -33,14 +28,11 @@ function handleMouseOut() {
   } else {
     // Si une note est sélectionnée, affiche les étoiles jusqu'à cette note
     for (let i = 0; i < stars.length; i++) {
-        if(stars[i].src = i < selectedRating){
-
-            stars[i].src= './img/star_pink.png'
-
-        }else{
-
-            stars[i].src ='./img/star_empty.png';
-        }
+      if (i < selectedRating) {
+        stars[i].src = './img/star_pink.png';
+      } else {
+        stars[i].src = './img/star_empty.png';
+      }
     }
   }
 }
@@ -51,18 +43,26 @@ function handleClick(index) {
   selectedRating = index + 1;
   // Change les étoiles jusqu'à celle cliquée
   for (let i = 0; i < stars.length; i++) {
-    if(stars[i].src = i < selectedRating){
-        stars[i].src = './img/star_pink.png'
-    }else{
-        stars[i].src = './img/star_empty.png';
+    if (i < selectedRating) {
+      stars[i].src = './img/star_pink.png';
+    } else {
+      stars[i].src = './img/star_empty.png';
     }
   }
 }
-// Fonction qui permet de reinitialiser les etoils
-function handleReset(){
-    selectedRating = 0;
-    stars.forEach((star) => {
-        star.src = './img/star_empty.png';
-    });
+
+// Fonction qui permet de réinitialiser les étoiles
+function handleReset() {
+  selectedRating = 0;
+  stars.forEach((star) => {
+    star.src = './img/star_empty.png';
+  });
 }
-btnReset.addEventListener('click', ()=> handleReset())
+
+// Ajouter des écouteurs d'événements pour chaque étoile
+btnReset.addEventListener('click', handleReset);
+stars.forEach((star, index) => {
+  star.addEventListener('mouseover', () => handleMouseOver(index));
+  star.addEventListener('mouseout', handleMouseOut);
+  star.addEventListener('click', () => handleClick(index));
+});
